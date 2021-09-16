edb10c7ddf8ece2bbc8312b75e282293f1f5e961
AlvaroMiranda1 <90862614+AlvaroMiranda1@users.noreply.github.com>
2021-09-16 20:22:37 -0300

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ConsoleApp7.model;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("┌────────────────────────────────────────────┐");
+            Console.WriteLine("│             TIENDA DE ARTICULOS            │");
+            Console.WriteLine("└────────────────────────────────────────────┘");
+            Console.WriteLine("¿Cuantos Articulos Agregara?");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            Tienda[] listaProductos = new Tienda[opcion];
+            for (int i = 0; i < opcion; i++)
+            {
+                Console.WriteLine($"Ingrese modelo del articulo {i+1}");
+                string modelo = Console.ReadLine();
+                Console.WriteLine($"Ingrese tipo del articulo {i+1}");
+                string tipo = Console.ReadLine();
+                Console.WriteLine($"Ingrese marca del articulo {i+1}");
+                string marca = Console.ReadLine();
+                Console.WriteLine($"Ingrese precio del articulo {i+1}");
+                int precio = Convert.ToInt32(Console.ReadLine());
+                Tienda newTienda = new Tienda(modelo, tipo, marca, precio);
+                listaProductos[i] = newTienda;
+            }
+            Console.WriteLine("┌────────────────────────────────────────────┐");
+            Console.WriteLine("│      Articulos guardados exitosamente      │");
+            Console.WriteLine("└────────────────────────────────────────────┘");
+            foreach (Tienda articulo in listaProductos)
+            {
+                Console.WriteLine($"el Articulo {articulo.Modelo} es un {articulo.Tipo} de la marca {articulo.Marca} y tiene un precio de {articulo.Precio}");
+            }
+        }
+    }
+}
---
+++ ConsoleApp7/ConsoleApp7/model/Tienda.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7.model
+{
+    class Tienda
+    {
+        private string modelo;
+        private string tipo;
+        private string marca;
+        private int precio;
+        public Tienda(string modelo,string tipo, string marca, int precio)
+        {
+            this.modelo = modelo;
+            this.tipo = tipo;
+            this.marca = marca;
+            this.precio = precio;
+        }
+
+        public string Tipo { get => tipo; set => tipo = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public int Precio { get => precio; set => precio = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+    }
+}
